7f2bec64fc4b55c20879ab0b02fe6ff6b13a1450
Lemon Nyzer <lemonyzer@gmail.com>
2015-01-02 13:23:18 +0100

    "CrystalQuest_2015.01.02.rar"

---
+++ Assets/Collisions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collisions : MonoBehaviour {
+
+	int points = 0;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Crystal")
+		{
+			points++;
+			Destroy(other.gameObject);
+		}
+		else if (other.gameObject.tag == "Enemy")
+		{
+			points = 0;
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Box("Points: " + points.ToString());
+	}
+}
