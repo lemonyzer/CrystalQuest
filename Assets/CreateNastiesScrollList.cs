963e26d62ec45eca7b9f7f4495e2887b353379b9
Lemon Nyzer <lemonyzer@gmail.com>
2015-04-26 03:52:26 +0200

    "CrystalQuest_2015.04.26_2_UI-GameModeSettings-ScrollView_Works_ContentSizeFitter_was_missing.rar"

---
+++ Assets/CreateNastiesScrollList.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Nasty {
+	public string name;
+	public Sprite icon;
+	public bool isOn;
+}
+
+public class CreateNastiesScrollList : MonoBehaviour {
+
+	public GameObject scrollViewContent;
+	public GameObject scrollContentPrefab;
+	public List<Nasty> nastiesList;
+
+	// Use this for initialization
+	void Start () {
+		PopulateList ();
+	}
+
+	void PopulateList ()
+	{
+		foreach(Nasty nasty in nastiesList)
+		{
+			GameObject newScrollContent = Instantiate (scrollContentPrefab) as GameObject;
+			NastyScrollItemScript itemScript = newScrollContent.GetComponent<NastyScrollItemScript>();
+			itemScript.itemTextName.text = nasty.name;
+			itemScript.itemImage.sprite = nasty.icon;
+			itemScript.itemToggle.isOn = nasty.isOn;
+			newScrollContent.transform.SetParent(scrollViewContent.transform,false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/NastyScrollItemScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NastyScrollItemScript : MonoBehaviour {
+
+	public Text itemTextName;
+	public Image itemImage;
+	public Toggle itemToggle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
