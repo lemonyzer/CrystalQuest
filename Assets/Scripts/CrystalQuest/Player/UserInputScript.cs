61066e05a3be2276e07e5ea855de98056b5e8096
Lemon Nyzer <lemonyzer@gmail.com>
2015-05-12 19:31:42 +0200

    "Crystal Quest UI Concept.rar"

---
+++ Assets/Scripts/CrystalQuest/Player/UserInputScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInputScript : MonoBehaviour {
+
+	private Vector2 m_Input;
+
+	public Vector2 GetInput()
+	{
+		return m_Input;
+	}
+
+	public float GetHorizontal()
+	{
+		return m_Input.x;
+	}
+
+	public float GetVertical()
+	{
+		return m_Input.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		m_Input.x = Input.GetAxis("Horizontal");
+		m_Input.y = Input.GetAxis("Vertical");
+
+	}
+}
