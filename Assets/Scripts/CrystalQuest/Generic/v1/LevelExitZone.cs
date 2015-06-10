c12dc320c92cf8b034fc2dad74a985326abf5a8e
Lemon Nyzer <lemonyzer@gmail.com>
2015-06-10 02:07:47 +0200

    "Crystal Quest UI Concept 2015.06.10_1.rar"

---
+++ Assets/Scripts/CrystalQuest/Generic/v1/LevelExitZone.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelExitZone : CollisionObject {
+	
+	[SerializeField]
+	protected bool sendCollisionDamageEnabled = false;
+
+	protected override void Awake ()
+	{
+		base.Awake ();
+		useCollisionEvents = true;	// Method A:
+									// Require: GO in Layer die nur mit Player GO reagiert.
+									// SendDamage = false
+	}
+
+	// Method B
+	// override OnTriggerEnter2D
+	// override OnCollisionEnter2D
+
+	// Method C
+	// override CollisionHandling2D
+	// override TriggerHandling2D
+
+	public override void ApplyCollisionDamage (CollisionObject otherObjectScript)
+	{
+		//base.ApplyCollisionDamage (otherObjectScript);
+		// no Damage
+	}
+
+	public override void SendCollisionDamage (CollisionObject otherObjectScript)
+	{
+		if (sendCollisionDamageEnabled)
+			base.SendCollisionDamage (otherObjectScript);
+	}
+
+
+}
--- Assets/Scripts/CrystalQuest/Generic/v1/LevelPortal.cs
+++ Assets/Scripts/CrystalQuest/Generic/v1/LevelPortal.cs
@@ -1,16 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class LevelPortal : CrystalQuestObjectScript {
+public class LevelPortal : CollisionObject {
 
-//	public override void ApplyCollisionDamage (CollisionObject otherObjectScript)
-//	{
-//		//base.ApplyCollisionDamage (otherObjectScript);
-//		// no Damage
-//	}
-//
-//	public override void SendCollisionDamage (CollisionObject otherObjectScript)
-//	{
-//		base.SendCollisionDamage (otherObjectScript);
-//	}
+	[SerializeField]
+	SpriteRenderer spriteRenderer;
+	[SerializeField]
+	Collider2D collider2d;
+	[SerializeField]
+	protected bool sendCollisionDamageEnabled = true;
+
+//	// TODO list of Objects that get enabled/disabled
+//	[SerializeField]
+//	protected List<Component> enableComponents;
+//	[SerializeField]
+//	protected List<Component> disableComponents;
+//	[SerializeField]
+//	protected List<Component> toggleComponents;
+
+	protected override void Awake ()
+	{
+		base.Awake ();
+		spriteRenderer = this.GetComponent<SpriteRenderer>();
+		collider2d = this.GetComponent<Collider2D>();
+	}
+
+	// TODO how to Test??!!
+
+	public void CloseGate ()
+	{
+		spriteRenderer.enabled = true;
+		sendCollisionDamageEnabled = true;
+		collider2d.enabled = true;
+	}
+
+	public void OpenGate ()
+	{
+//		foreach (Component component in toggleComponents)
+//		{
+//			Debug.Log(component.GetType ());
+//		}
+		spriteRenderer.enabled = false;
+		sendCollisionDamageEnabled = false;
+		collider2d.enabled = false;
+	}
+
+	public override void ApplyCollisionDamage (CollisionObject otherObjectScript)
+	{
+		//base.ApplyCollisionDamage (otherObjectScript);
+		// no Damage
+	}
+
+	public override void SendCollisionDamage (CollisionObject otherObjectScript)
+	{
+		if (sendCollisionDamageEnabled)
+			base.SendCollisionDamage (otherObjectScript);
+	}
 }
